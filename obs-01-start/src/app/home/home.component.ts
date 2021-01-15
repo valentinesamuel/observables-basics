0ed18d79e7e5e09b8e6f7345124e5204e73eeaa6
Valentine <valentinesamuel2580@gmail.com>
2021-01-15 16:04:46 +0100

    (BASIC):First commit here according to the given course file

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { interval, Subscription } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private firstObsSubscription: Subscription;
+export class HomeComponent implements OnInit {
+
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((increment) => {
-      console.log(increment);
-    });
+    
   }
 
-  ngOnDestroy() {
-    this.firstObsSubscription.unsubscribe();
-  }
+ 
 }
