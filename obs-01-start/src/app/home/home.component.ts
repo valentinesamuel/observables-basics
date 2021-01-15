a3954658f293263b99dac8d1a2b2c695fce5fa19
Valentine <valentinesamuel2580@gmail.com>
2021-01-15 17:41:31 +0100

    (FEAT):Added an error handling code block inside the setInterval method

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription, interval, Observable } from "rxjs";
 
-
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -10,7 +9,7 @@ import { Subscription, interval, Observable } from "rxjs";
 export class HomeComponent implements OnInit, OnDestroy {
   firstObserver: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     // this.firstObserver = interval(1000).subscribe(
@@ -18,21 +17,23 @@ export class HomeComponent implements OnInit, OnDestroy {
     //     console.log(increment);
     //   }
     // )
-    const scratchObserver = new Observable(observer => {
+    const scratchObserver = new Observable((observer) => {
       let count = 0;
       setInterval(() => {
         count++;
         observer.next(count);
+        if (count > 3) {
+          observer.error(Error("Your count is greater than 3"));
+        }
       }, 1000);
     });
 
-    this.firstObserver = scratchObserver.subscribe(data => {
+    this.firstObserver = scratchObserver.subscribe((data) => {
       console.log(data);
     });
   }
 
   ngOnDestroy() {
     this.firstObserver.unsubscribe();
-}
- 
+  }
 }
